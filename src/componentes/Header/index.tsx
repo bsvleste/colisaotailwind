226c985e731362fc09ddf807bab88fe8f3233446
bsvleste <bvaleiro@gmail.com>
2022-08-31 22:15:29 -0300

    configurando o projeto com tailwind

---
+++ src/componentes/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header(){
+    return(
+        <div className="w-auto bg-colisao-500 h-14">
+            <div className="container mx-auto h-14 flex justify-between">
+                <div className="flex items-center space-x-6">
+                    <div className="">Resultado</div>
+                    <div className="">bid</div>                
+                </div>
+            </div>
+        </div>
+    )
+
+}
