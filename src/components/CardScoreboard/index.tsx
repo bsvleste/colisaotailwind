153b123aaabe27ddd13766748772e39a8e6e82e8
bsvleste <bvaleiro@gmail.com>
2022-09-11 19:11:59 -0300

    adicionado loading

--- src/components/CardScoreboard/index.tsx
+++ src/components/CardScoreboard/index.tsx
@@ -19,40 +19,48 @@ export interface ScoreboardMatchProps {
   }
 export function CardScoreboard(){
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [getIdToUpdate, setGetIdToUpdate] = useState('');
     const [scoreboard, setScoreboard] = useState<ScoreboardMatchProps[]>([]);
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const [title, setTitle] = useState('');
-    async function getScoreboard() {
-      const {data} = await api.get('/placar');
-      /* const res = localStorage.getItem('@colisao'); */
+/*     const [data,setData] = useState([]);
+ */    async function getScoreboard() {
+       const {data} = await  api.get('/placar');
+        setIsLoading(false)      
       const resultsMont = data.filter(
         (res: ScoreboardMatchProps) =>
-          new Date(res.dataPartida).getMonth() === selectedDate.getMonth() &&
-          new Date(res.dataPartida).getFullYear() === selectedDate.getFullYear()
-      ); 
-       setScoreboard(resultsMont);
-    }
-    useEffect(() => {
-      getScoreboard();
-  
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectedDate, isModalOpen]);
-  
+        new Date(res.dataPartida).getMonth() === selectedDate.getMonth() &&
+        new Date(res.dataPartida).getFullYear() === selectedDate.getFullYear()
+        ); 
+        setScoreboard(resultsMont);
+      }
+      useEffect(() => {
+        getScoreboard();
+        
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [selectedDate, isModalOpen]);
+      
     function handleChangeDate(action: 'next' | 'prev') {
       if (action === 'next') {
         setSelectedDate(addMonths(selectedDate, 1));
-    } else {
+        setIsLoading(!isLoading)
+      } else {
         setSelectedDate(subMonths(selectedDate, 1));
+        setIsLoading(!isLoading)
       }
     }
     return (
         <Container>
             <Button />              
             <SelectedMonth selectedDate={selectedDate} handleChangeDate={handleChangeDate}/>
-                {scoreboard.map((data)=>(
-                     <CardMatch key={data._id} info={data}/>                                     
-                 ))}            
+
+                  {
+                    isLoading ? <h1>Ola</h1>:scoreboard.map((data)=>(
+                      <CardMatch key={data._id} info={data}/>                                     
+                    ))   
+                  }           
+                     
+                 
         </Container>
     )
 }
